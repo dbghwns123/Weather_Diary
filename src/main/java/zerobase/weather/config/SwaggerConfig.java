aa4fd8720ff505df41001bc4fecb2062fb249d64
Ho-JunYu <hojoon1011@gmail.com>
2025-01-02 03:57:09 +0900

    swagger 실습

---
+++ src/main/java/zerobase/weather/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package zerobase.weather.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Example API")
+                        .version("1.0.0")
+                        .description("This is an example API documentation.")
+                        .contact(new Contact()
+                                .name("Hojun")
+                                .email("hojun@example.com")
+                                .url("https://example.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Full Documentation")
+                        .url("https://example.com/docs"));
+    }
+}
